99c9d090e40ea7675f33c4347f47fdef7c65b086
Tianhe Wu <tianhe_wu@foxmail.com>
2021-10-10 17:15:41 +0800

    move gitignore and add dynamic programming

---
+++ Algorithm_Practice/leetcode/309.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int dp[n][3];
+        dp[0][0] = 0, dp[0][1] = -prices[0], dp[0][2] = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][1] = max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+            dp[i][2] = dp[i - 1][1] + prices[i];
+        }
+        return max(dp[n - 1][0], dp[n - 1][2]);
+    }
+};
